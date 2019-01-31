590381f32fa8106f6ab4953b8207243a8df1b724
Ryan <hello@ryanwu.me>
2019-01-31 16:12:39 -0500

    1.1.0 added date support

--- test/parser.test.js
+++ test/parser.test.js
@@ -34,24 +34,44 @@ describe('parser usage', () => {
       .expect({ test: ['a', 'b', 'c'] })
       .end(done)
   })
-  test('should be able to use custom parser', (done) => {
-    sampleApp.get('/route3', getParam('test', { parser: (v) => (parseInt(v, 10) * 100) }), (req, res) => {
+  test('should get date value ', (done) => {
+    sampleApp.get('/route3', getParam('testdate', { parser: getParam.DATE }), (req, res) => {
       res.json(res.locals)
     })
     supertest(sampleApp)
-      .get('/route3?test=5.2')
+      .get('/route3?testdate=2019-01-24')
+      .expect(200)
+      .expect({ testdate: '2019-01-24T05:00:00.000Z' })
+      .end(done)
+  })
+  test('should get date value with specific format ', (done) => {
+    sampleApp.get('/route3', getParam('testdate', { parser: getParam.DATE, dateFormat: 'DD-MM-YYYY' }), (req, res) => {
+      res.json(res.locals)
+    })
+    supertest(sampleApp)
+      .get('/route3?testdate=02-01-2019')
+      .expect(200)
+      .expect({ testdate: '2019-01-02T05:00:00.000Z' })
+      .end(done)
+  })
+  test('should be able to use custom parser', (done) => {
+    sampleApp.get('/route4', getParam('test', { parser: (v) => (parseInt(v, 10) * 100) }), (req, res) => {
+      res.json(res.locals)
+    })
+    supertest(sampleApp)
+      .get('/route4?test=5.2')
       .expect(200)
       .expect({ test: 500 })
       .end(done)
   })
   test('should response empty if parse error', (done) => {
-    sampleApp.get('/route4', getParam('test', {
+    sampleApp.get('/route5', getParam('test', {
       parser: getParam.INTEGER
     }), (req, res) => {
       res.json(res.locals)
     })
     supertest(sampleApp)
-      .get('/route4?test=string')
+      .get('/route5?test=string')
       .expect(200)
       .expect({})
       .end(done)
